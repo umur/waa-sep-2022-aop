5dc82950cb7c9ecacd7097240bb63616e35a2ab5
WEDNESDAYfate <g.lkhagvazaya@gmail.com>
2022-09-09 22:03:25 -0500

    aop

---
+++ Spring-aop/src/main/java/edu/miu/demo/model/ActivityLog.java
@@ -0,0 +1,30 @@
+package edu.miu.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class ActivityLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private Date date;
+    private String operation;
+    private double duration;
+
+    public ActivityLog(Date date, String operation, long duration) {
+        this.getId();
+        this.date = date;
+        this.operation = operation;
+        this.duration = duration;
+    }
+}
