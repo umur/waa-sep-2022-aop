f6828af4a7338ec37e070f8c26337a9a29483e6d
Abdulhakim-Edao <abdulhakim.edao1@gmail.com>
2022-09-09 23:53:18 -0500

    sql data added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO student (id,name) VALUES
+
+    (3,'Maisie Peters'),
+    (8,'Nigel Oconnor'),
+    (1,'Ryder Hays'),
+    (10,'Nayda Hurst'),
+    (9,'Clayton Mclean'),
+    (2,'Joseph Welch'),
+    (4,'Bradley Castillo'),
+    (12,'Harrison Marks'),
+    (5,'Allen Oliver');
